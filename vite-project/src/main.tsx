244f824f442881cc3368899d7bcb56ec049ed9ad
Catcathg <catherine-guo@outlook.fr>
2024-08-12 02:07:49 +0200

    init

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement);
