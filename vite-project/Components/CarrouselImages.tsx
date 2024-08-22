969e2b62afc27aa0e7876846ad1b51d3700a6c28
Catcathg <catherine-guo@outlook.fr>
2024-08-22 23:13:22 +0200

    init

--- vite-project/Components/CarrouselImages.tsx
+++ vite-project/Components/CarrouselImages.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Box, Grid, IconButton } from '@mui/material';
 import { styled } from '@mui/system';
 
